d3d64f72e09d4e2101e3e9e05ff5caa061014a2a
bunn1 <nicjohbun@gmail.com>
2022-04-30 21:06:34 +0200

    module.exports summary

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 const tutorial = require('./tutorial')
-console.log(tutorial)
+console.log(tutorial.sum(1,1));
+console.log(tutorial.PI)
+console.log(new tutorial.SomeMathObject())
+
+
--- tutorial.js
+++ tutorial.js
@@ -7,6 +7,5 @@ class SomeMathObject{
 }
 
 // Funktionen är redo för att användas
-module.exports.sum = sum;
-module.exports.PI = PI;
-module.exports.SomeMathObject = SomeMathObject;
+module.exports = {sum : sum, PI : PI, SomeMathObject : SomeMathObject}
+
